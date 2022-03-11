330e47e1d0040d662bf7721d0ee9d648ed367fc8
Parvej khan <kparvej761@gmail.com>
2022-03-11 22:29:34 +0530

    leaning while & For loop

---
+++ Day14/js_while_loop.js
@@ -0,0 +1,11 @@
+// Setup Add the numbers 5 through 0 (inclusive) in descending order to myArray using a while loop.
+const myArray = [];
+
+// Only change code below this line
+let i = 5;
+  while (i >= 0){
+  myArray.push(i);
+  i--;
+}
+
+
